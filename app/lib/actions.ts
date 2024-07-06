1e8e0f43667db69d3b20dfa7b9f38b15ca83f5a7
N1kk1337 <nikillerok@gmail.com>
2024-07-06 16:08:41 +0300

    feat: add invoice update functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,7 +7,7 @@ import { redirect } from 'next/navigation';
 
 
 
-const FormScheme = z.object({
+const FormSchema = z.object({
     id: z.string(),
     customerId: z.string(),
     amount: z.coerce.number(),
@@ -15,7 +15,9 @@ const FormScheme = z.object({
     date: z.string(),
 });
 
-const CreateInvoice = FormScheme.omit({ id:true, date:true});
+const CreateInvoice = FormSchema.omit({ id: true, date: true});
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 
 export async function createInvoice(formData:FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
@@ -34,3 +36,23 @@ export async function createInvoice(formData:FormData) {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
+
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
