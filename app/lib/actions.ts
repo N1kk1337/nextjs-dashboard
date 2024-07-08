579483dc2d61eb640bf1e0d467464711fb63707a
N1kk1337 <nikillerok@gmail.com>
2024-07-08 23:02:47 +0300

    feat: improve error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,7 +5,14 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
 const FormSchema = z.object({
     id: z.string(),
@@ -23,15 +30,25 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true});
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 
-export async function createInvoice(formData:FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status')
     });
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
-    try {
+
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Create Invoice.',
+      };
+    }
+
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  try {
         await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
